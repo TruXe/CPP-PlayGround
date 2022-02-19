cef23a4bb5f2268e144bb501b651ff54b77bb370
TruXe <48220922+TruXe@users.noreply.github.com>
2022-02-19 14:10:14 +0100

    1

--- CPP-PlayGround/CPP-PlayGround/CPP-PlayGround.cpp
+++ CPP-PlayGround/CPP-PlayGround/CPP-PlayGround.cpp
@@ -1,6 +1,31 @@
 ﻿#include <iostream>
+#include "windows.h"
+#include <vector>
 
+int X = 11;
+int Y = 12;
+int __MATH__ = X + Y; // 23
+
+
+int title()
+{
+	SetConsoleTitleA("CPP Playground");
+	return 1;
+}
+
+//MATHS
 int main()
 {
-    std::cout << "Hello World!\n";
+	title();
+	if (__MATH__ == 23)
+	{
+		std::cout << "Math: " << __MATH__;
+		std::cout << "\n";
+		system("pause");
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
