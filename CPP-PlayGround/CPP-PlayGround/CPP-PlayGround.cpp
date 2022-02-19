175b07d45767752f9b4053456c3f3567283a9ddc
TruXe <48220922+TruXe@users.noreply.github.com>
2022-02-19 14:28:11 +0100

    2

--- CPP-PlayGround/CPP-PlayGround/CPP-PlayGround.cpp
+++ CPP-PlayGround/CPP-PlayGround/CPP-PlayGround.cpp
@@ -1,10 +1,13 @@
 ﻿#include <iostream>
 #include "windows.h"
 #include <vector>
+#include <sstream>
 
 int X = 11;
 int Y = 12;
-int __MATH__ = X + Y; // 23
+
+int __MATH__INT = X + Y; // 23
+
 
 
 int title()
@@ -13,15 +16,41 @@ int title()
 	return 1;
 }
 
+int captcha__ = { 0 };
+
 //MATHS
 int main()
 {
 	title();
-	if (__MATH__ == 23)
+	if (__MATH__INT == 23)
 	{
-		std::cout << "Math: " << __MATH__;
+		std::cout << "Math: " << __MATH__INT;
 		std::cout << "\n";
-		system("pause");
+		bool escape_;
+		std::string __TYPE;
+		std::cout << "BOOL: "; std::cin >> __TYPE;
+
+		if (__TYPE == "true")
+		{
+			
+			escape_ = true;
+			std::cout << escape_;
+			system("cls");
+			std::cout << "App stay running." << "\n\n";;
+			system("pause");
+			return 1;
+
+		}
+		else
+		{
+			
+			escape_ = false;
+			std::cout << escape_ << "\n\n";
+			std::cout << "App turning off." << "\n\n";;
+			system("pause");
+			return 0;
+		}
+
 		return 1;
 	}
 	else
